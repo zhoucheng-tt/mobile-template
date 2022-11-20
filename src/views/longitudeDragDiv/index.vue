705c2012f0c92b7a1855829faedac769662bcf0d
zhoucheng <2315741635@qq.com>
2022-11-20 22:09:57 +0800

    插件实现div拖拽

--- src/views/longitudeDragDiv/index.vue
+++ src/views/longitudeDragDiv/index.vue
@@ -11,20 +11,61 @@
                  left-text="返回"
                  left-arrow
                  @click-left="handleClickTitleLeft" />
+    <div class="content">
+      <draggable v-model="dataList"
+                 @end="end">
+        <transition-group>
+          <div v-for="item in dataList"
+               :key="item.key"
+               class="item">
+            {{item.value}}
+          </div>
+        </transition-group>
+      </draggable>
+    </div>
   </div>
 </template>
 
 <script>
 // 这里可以导入其他文件（比如：组件，工具js，第三方插件js，json文件，图片文件等等）
 // 例如：import 《组件名称》 from '《组件路径》';
-
+import draggable from 'vuedraggable'
 export default {
   // import引入的组件需要注入到对象中才能使用
-  components: {},
+  components: {
+    draggable
+  },
   data () {
     // 这里存放数据
     return {
-      titleName: ''
+      titleName: '',
+      dataList: [
+        {
+          key: 1,
+          value: 1,
+          height: '100px'
+        },
+        {
+          key: 2,
+          value: 2,
+          height: '110px'
+        },
+        {
+          key: 3,
+          value: 3,
+          height: '80px'
+        },
+        {
+          key: 4,
+          value: 4,
+          height: '50px'
+        },
+        {
+          key: 5,
+          value: 5,
+          height: '60px'
+        }
+      ]
     }
   },
   // 监听属性 类似于data概念
@@ -42,6 +83,13 @@ export default {
   },
   // 方法集合
   methods: {
+    dragstart (val) {
+      console.log('触发')
+      // console.log(val)
+    },
+    end () {
+      console.log(this.dataList)
+    },
     // 点击返回
     handleClickTitleLeft () {
       this.$router.go(-1)
@@ -63,5 +111,22 @@ export default {
   height: 100%;
   overflow-x: hidden;
   overflow-y: auto;
+  .content {
+    width: 100%;
+    height: calc(100% - 46px);
+    overflow-x: hidden;
+    overflow-y: auto;
+    .item {
+      width: 94%;
+      margin-left: 3%;
+      margin-top: 10px;
+      height: 40px;
+      background: greenyellow;
+      color: #000000;
+      font-size: 20px;
+      text-align: center;
+      line-height: 40px;
+    }
+  }
 }
 </style>
